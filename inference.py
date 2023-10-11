d84d144c39aab966e87a85b01f11bf78dfc8c1de
90houlaoheshang <907333918@qq.com>
2023-10-11 22:23:21 +0800

    重构代码结构

--- inference.py
+++ inference.py
@@ -1,5 +1,5 @@
 from utils.deep_speech import DeepSpeech
-from utils.data_processing import load_landmark_openface,compute_crop_radius
+from utils.data_processing import load_landmark_openface, compute_crop_radius
 from config.config import DINetInferenceOptions
 from models.DINet import DINet
 
@@ -12,7 +12,8 @@ import subprocess
 import random
 from collections import OrderedDict
 
-def extract_frames_from_video(video_path,save_dir):
+
+def extract_frames_from_video(video_path, save_dir):
     videoCapture = cv2.VideoCapture(video_path)
     fps = videoCapture.get(cv2.CAP_PROP_FPS)
     if int(fps) != 25:
@@ -24,7 +25,8 @@ def extract_frames_from_video(video_path,save_dir):
         ret, frame = videoCapture.read()
         result_path = os.path.join(save_dir, str(i).zfill(6) + '.jpg')
         cv2.imwrite(result_path, frame)
-    return (int(frame_width),int(frame_height))
+    return (int(frame_width), int(frame_height))
+
 
 if __name__ == '__main__':
     # load config
@@ -36,7 +38,7 @@ if __name__ == '__main__':
     video_frame_dir = opt.source_video_path.replace('.mp4', '')
     if not os.path.exists(video_frame_dir):
         os.mkdir(video_frame_dir)
-    video_size = extract_frames_from_video(opt.source_video_path,video_frame_dir)
+    video_size = extract_frames_from_video(opt.source_video_path, video_frame_dir)
     ############################################## extract deep speech feature ##############################################
     print('extracting deepspeech feature from : {}'.format(opt.driving_audio_path))
     if not os.path.exists(opt.deepspeech_model_path):
@@ -68,7 +70,8 @@ if __name__ == '__main__':
         divisor = res_frame_length // video_frame_path_list_cycle_length
         remainder = res_frame_length % video_frame_path_list_cycle_length
         res_video_frame_path_list = video_frame_path_list_cycle * divisor + video_frame_path_list_cycle[:remainder]
-        res_video_landmark_data = np.concatenate([video_landmark_data_cycle]* divisor + [video_landmark_data_cycle[:remainder, :, :]],0)
+        res_video_landmark_data = np.concatenate(
+            [video_landmark_data_cycle] * divisor + [video_landmark_data_cycle[:remainder, :, :]], 0)
     res_video_frame_path_list_pad = [video_frame_path_list_cycle[0]] * 2 \
                                     + res_video_frame_path_list \
                                     + [video_frame_path_list_cycle[-1]] * 2
@@ -83,17 +86,19 @@ if __name__ == '__main__':
     resize_h = int((opt.mouth_region_size // 2) * 3 + opt.mouth_region_size // 8)
     ref_index_list = random.sample(range(5, len(res_video_frame_path_list_pad) - 2), 5)
     for ref_index in ref_index_list:
-        crop_flag,crop_radius = compute_crop_radius(video_size,res_video_landmark_data_pad[ref_index - 5:ref_index, :, :])
+        crop_flag, crop_radius = compute_crop_radius(video_size,
+                                                     res_video_landmark_data_pad[ref_index - 5:ref_index, :, :])
         if not crop_flag:
             raise ('our method can not handle videos with large change of facial size!!')
         crop_radius_1_4 = crop_radius // 4
-        ref_img = cv2.imread(res_video_frame_path_list_pad[ref_index- 3])[:, :, ::-1]
+        ref_img = cv2.imread(res_video_frame_path_list_pad[ref_index - 3])[:, :, ::-1]
         ref_landmark = res_video_landmark_data_pad[ref_index - 3, :, :]
         ref_img_crop = ref_img[
-                  ref_landmark[29, 1] - crop_radius:ref_landmark[29, 1] + crop_radius * 2 + crop_radius_1_4,
-                  ref_landmark[33, 0] - crop_radius - crop_radius_1_4:ref_landmark[33, 0] + crop_radius +crop_radius_1_4,
-                  :]
-        ref_img_crop = cv2.resize(ref_img_crop,(resize_w,resize_h))
+                       ref_landmark[29, 1] - crop_radius:ref_landmark[29, 1] + crop_radius * 2 + crop_radius_1_4,
+                       ref_landmark[33, 0] - crop_radius - crop_radius_1_4:ref_landmark[
+                                                                               33, 0] + crop_radius + crop_radius_1_4,
+                       :]
+        ref_img_crop = cv2.resize(ref_img_crop, (resize_w, resize_h))
         ref_img_crop = ref_img_crop / 255.0
         ref_img_list.append(ref_img_crop)
     ref_video_frame = np.concatenate(ref_img_list, 2)
@@ -114,7 +119,8 @@ if __name__ == '__main__':
     ############################################## inference frame by frame ##############################################
     if not os.path.exists(opt.res_video_dir):
         os.mkdir(opt.res_video_dir)
-    res_video_path = os.path.join(opt.res_video_dir,os.path.basename(opt.source_video_path)[:-4] + '_facial_dubbing.mp4')
+    res_video_path = os.path.join(opt.res_video_dir,
+                                  os.path.basename(opt.source_video_path)[:-4] + '_facial_dubbing.mp4')
     if os.path.exists(res_video_path):
         os.remove(res_video_path)
     res_face_path = res_video_path.replace('_facial_dubbing.mp4', '_synthetic_face.mp4')
@@ -124,35 +130,40 @@ if __name__ == '__main__':
     videowriter_face = cv2.VideoWriter(res_face_path, cv2.VideoWriter_fourcc(*'XVID'), 25, (resize_w, resize_h))
     for clip_end_index in range(5, pad_length, 1):
         print('synthesizing {}/{} frame'.format(clip_end_index - 5, pad_length - 5))
-        crop_flag, crop_radius = compute_crop_radius(video_size,res_video_landmark_data_pad[clip_end_index - 5:clip_end_index, :, :],random_scale = 1.05)
+        crop_flag, crop_radius = compute_crop_radius(video_size,
+                                                     res_video_landmark_data_pad[clip_end_index - 5:clip_end_index, :,
+                                                     :], random_scale=1.05)
         if not crop_flag:
             raise ('our method can not handle videos with large change of facial size!!')
         crop_radius_1_4 = crop_radius // 4
         frame_data = cv2.imread(res_video_frame_path_list_pad[clip_end_index - 3])[:, :, ::-1]
         frame_landmark = res_video_landmark_data_pad[clip_end_index - 3, :, :]
         crop_frame_data = frame_data[
-                            frame_landmark[29, 1] - crop_radius:frame_landmark[29, 1] + crop_radius * 2 + crop_radius_1_4,
-                            frame_landmark[33, 0] - crop_radius - crop_radius_1_4:frame_landmark[33, 0] + crop_radius +crop_radius_1_4,
-                            :]
-        crop_frame_h,crop_frame_w = crop_frame_data.shape[0],crop_frame_data.shape[1]
-        crop_frame_data = cv2.resize(crop_frame_data, (resize_w,resize_h))  # [32:224, 32:224, :]
+                          frame_landmark[29, 1] - crop_radius:frame_landmark[29, 1] + crop_radius * 2 + crop_radius_1_4,
+                          frame_landmark[33, 0] - crop_radius - crop_radius_1_4:frame_landmark[
+                                                                                    33, 0] + crop_radius + crop_radius_1_4,
+                          :]
+        crop_frame_h, crop_frame_w = crop_frame_data.shape[0], crop_frame_data.shape[1]
+        crop_frame_data = cv2.resize(crop_frame_data, (resize_w, resize_h))  # [32:224, 32:224, :]
         crop_frame_data = crop_frame_data / 255.0
-        crop_frame_data[opt.mouth_region_size//2:opt.mouth_region_size//2 + opt.mouth_region_size,
-                        opt.mouth_region_size//8:opt.mouth_region_size//8 + opt.mouth_region_size, :] = 0
+        crop_frame_data[opt.mouth_region_size // 2:opt.mouth_region_size // 2 + opt.mouth_region_size,
+        opt.mouth_region_size // 8:opt.mouth_region_size // 8 + opt.mouth_region_size, :] = 0
 
         crop_frame_tensor = torch.from_numpy(crop_frame_data).float().cuda().permute(2, 0, 1).unsqueeze(0)
-        deepspeech_tensor = torch.from_numpy(ds_feature_padding[clip_end_index - 5:clip_end_index, :]).permute(1, 0).unsqueeze(0).float().cuda()
+        deepspeech_tensor = torch.from_numpy(ds_feature_padding[clip_end_index - 5:clip_end_index, :]).permute(1,
+                                                                                                               0).unsqueeze(
+            0).float().cuda()
         with torch.no_grad():
             pre_frame = model(crop_frame_tensor, ref_img_tensor, deepspeech_tensor)
             pre_frame = pre_frame.squeeze(0).permute(1, 2, 0).detach().cpu().numpy() * 255
         videowriter_face.write(pre_frame[:, :, ::-1].copy().astype(np.uint8))
-        pre_frame_resize = cv2.resize(pre_frame, (crop_frame_w,crop_frame_h))
+        pre_frame_resize = cv2.resize(pre_frame, (crop_frame_w, crop_frame_h))
         frame_data[
         frame_landmark[29, 1] - crop_radius:
         frame_landmark[29, 1] + crop_radius * 2,
         frame_landmark[33, 0] - crop_radius - crop_radius_1_4:
         frame_landmark[33, 0] + crop_radius + crop_radius_1_4,
-        :] = pre_frame_resize[:crop_radius * 3,:,:]
+        :] = pre_frame_resize[:crop_radius * 3, :, :]
         videowriter.write(frame_data[:, :, ::-1])
     videowriter.release()
     videowriter_face.release()
@@ -164,10 +175,3 @@ if __name__ == '__main__':
         opt.driving_audio_path,
         video_add_audio_path)
     subprocess.call(cmd, shell=True)
-
-
-
-
-
-
-

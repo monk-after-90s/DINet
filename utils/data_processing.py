276b993306eaad64d9c1cba477ea67388cb41525
90houlaoheshang <907333918@qq.com>
2023-10-12 15:37:22 +0800

    代码注解

--- utils/data_processing.py
+++ utils/data_processing.py
@@ -29,16 +29,16 @@ def compute_crop_radius(video_size,landmark_data_clip,random_scale = None):
     judge if crop face and compute crop radius
     '''
     video_w, video_h = video_size[0], video_size[1]
-    landmark_max_clip = np.max(landmark_data_clip, axis=1)
+    landmark_max_clip = np.max(landmark_data_clip, axis=1)#每张人脸图的边框右下角
     if random_scale is None:
         random_scale = random.random() / 10 + 1.05
     else:
         random_scale = random_scale
-    radius_h = (landmark_max_clip[:,1] - landmark_data_clip[:,29, 1]) * random_scale
-    radius_w = (landmark_data_clip[:,54, 0] - landmark_data_clip[:,48, 0]) * random_scale
-    radius_clip = np.max(np.stack([radius_h, radius_w],1),1) // 2
-    radius_max = np.max(radius_clip)
-    radius_max = (np.int(radius_max/4) + 1 ) * 4
+    radius_h = (landmark_max_clip[:,1] - landmark_data_clip[:,29, 1]) * random_scale#每张脸的鼻子中部到人脸图的边框下边的距离
+    radius_w = (landmark_data_clip[:,54, 0] - landmark_data_clip[:,48, 0]) * random_scale#每张脸的嘴巴两侧的距离
+    radius_clip = np.max(np.stack([radius_h, radius_w],1),1) // 2#每张图的鼻子和嘴巴长块绘圆的半径
+    radius_max = np.max(radius_clip)#最大的半径
+    radius_max = (np.int(radius_max/4) + 1 ) * 4#往上取4倍整
     radius_max_1_4 = radius_max//4
     clip_min_h = landmark_data_clip[:, 29,
                  1] - radius_max
